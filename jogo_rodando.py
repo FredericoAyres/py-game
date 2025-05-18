c50384c5f35bef9f73f2efa731a669802d614164
Frederico <fredericoma@al.insper.edu.br>
2025-05-17 21:06:06 -0300

    chuta e defende

--- jogo_rodando.py
+++ jogo_rodando.py
@@ -25,45 +25,102 @@ def jogo_rodando(window):
     image_5 = pygame.image.load('assets/img/5_DesSoft.png').convert_alpha()
     image_5 = pygame.transform.scale(image_5,(400, 400))
 
+    font = pygame.font.SysFont(None, 48)
+
     goleiro = Goleiro(image_goleiro)
     jogador = Jogador(image_jogador)
     bola = Bola(image_bola)
+
+    turno = "chute"
+    esperando_chute = True
 
     while running:
 
         clock.tick(60)
 
-        for event in pygame.event.get(): #devolve uma lista de eventos
+        for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 running = False
                 estado = QUIT
-            
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_1:
-                    jogador.direcao = 1
-                    bola.direcao = 1
-                    goleiro.direcao = random.randint(1, 5)
-                if event.key == pygame.K_2:
-                    jogador.direcao = 2
-                    bola.direcao = 2
-                    goleiro.direcao = random.randint(1, 5)
-                if event.key == pygame.K_3:
-                    jogador.direcao = 3
-                    bola.direcao = 3
-                    goleiro.direcao = random.randint(1, 5)
-                if event.key == pygame.K_4:
-                    jogador.direcao = 4
-                    bola.direcao = 4
-                    goleiro.direcao = random.randint(1, 5)
-                if event.key == pygame.K_5:
-                    jogador.direcao = 5
-                    bola.direcao = 5
-                    goleiro.direcao = random.randint(1, 5)
+
+            if esperando_chute and event.type == pygame.KEYUP:
+                if turno == "chute":
+                    if event.key == pygame.K_1:
+                        jogador.direcao = 1
+                        bola.direcao = 1
+                        goleiro.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_2:
+                        jogador.direcao = 2
+                        bola.direcao = 2
+                        goleiro.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_3:
+                        jogador.direcao = 3
+                        bola.direcao = 3
+                        goleiro.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_4:
+                        jogador.direcao = 4
+                        bola.direcao = 4
+                        goleiro.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_5:
+                        jogador.direcao = 5
+                        bola.direcao = 5
+                        goleiro.direcao = random.randint(1, 5)
+                        esperando_chute = False
+
+                elif turno == "defesa":
+                    if event.key == pygame.K_1:
+                        goleiro.direcao = 1
+                        jogador.direcao = 3
+                        bola.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_2:
+                        goleiro.direcao = 2
+                        jogador.direcao = 3
+                        bola.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_3:
+                        goleiro.direcao = 3
+                        jogador.direcao = 3
+                        bola.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_4:
+                        goleiro.direcao = 4
+                        jogador.direcao = 3
+                        bola.direcao = random.randint(1, 5)
+                        esperando_chute = False
+                    if event.key == pygame.K_5:
+                        goleiro.direcao = 5
+                        jogador.direcao = 3
+                        bola.direcao = random.randint(1, 5)
+                        esperando_chute = False
 
         goleiro.update()       
         jogador.update()
         bola.update()
+
+        if bola.direcao == 0 and jogador.direcao == 0 and goleiro.direcao == 0 and not esperando_chute:
+            pygame.time.wait(1000)
+            if turno == "chute":
+                turno = "defesa"
+            else:
+                turno = "chute"
+            esperando_chute = True
+            jogador.rect.x = 220
+            jogador.rect.y = 450
+            goleiro.rect.x = 315
+            goleiro.rect.y = 220
+            bola.rect.x = 375
+            bola.rect.y = 470
+
+        if turno == "chute":
+            text = font.render('Sua vez de chutar!', True, (255, 255, 255))
+        else:
+            text = font.render('Sua vez de defender!', True, (255, 255, 255))
 
         window.blit(image_fundo, (0, 0))
         window.blit(image_goleiro, (goleiro.rect.x, goleiro.rect.y))
@@ -74,8 +131,9 @@ def jogo_rodando(window):
         window.blit(image_3, (200, -30))
         window.blit(image_4, (480, -30))
         window.blit(image_5, (480, 100))
+        window.blit(text,(10, 10))
 
         pygame.display.flip()
         pygame.display.update()
 
-    return estado
+    return estado 
