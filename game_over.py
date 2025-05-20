9a10608793731026cc1d4a43407f4553334ec1a1
Frederico <fredericoma@al.insper.edu.br>
2025-05-20 18:24:11 -0300

    botões de sim e não

--- game_over.py
+++ game_over.py
@@ -4,25 +4,34 @@ from config import INIT, QUIT, GAME, GAME_OVER
 def game_over(window):
     running = True
     clock = pygame.time.Clock()
+
     image_game_over = pygame.image.load('assets/img/Game_over.jpg').convert_alpha()
     image_game_over = pygame.transform.scale(image_game_over, (800, 600))
+
+    # Define as áreas dos botões manualmente com base na imagem
+    botao_sim = pygame.Rect(215, 500, 150, 60)   # x, y, largura, altura
+    botao_nao = pygame.Rect(440, 500, 150, 60)
+
+    estado = GAME_OVER
+
     while running:
         clock.tick(60)
 
-        for event in pygame.event.get(): #devolve uma lista de eventos
-
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
                 estado = QUIT
-            
-            if event.type == pygame.KEYUP:
                 running = False
-                estado = GAME
 
-        window.blit(image_game_over,(0, 0))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if botao_sim.collidepoint(event.pos):
+                    estado = INIT
+                    running = False
 
+                if botao_nao.collidepoint(event.pos):
+                    estado = QUIT
+                    running = False
+
+        window.blit(image_game_over, (0, 0))
         pygame.display.update()
 
-    estado = GAME_OVER
-    
     return estado
